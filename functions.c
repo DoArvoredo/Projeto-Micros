b3494f6b56767086be89eb95b989a8b7274ccffa
DoArvoredo <39414989+DoArvoredo@users.noreply.github.com>
2018-05-18 14:50:39 -0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -102,6 +102,46 @@ char varredura_teclado()
 
 }return TECLA;
 
+//setup timer 0
+void setup_timer0()
+{
+	TMOD = 2;
+	TH0 = 163;
+	TL0 = 163;
+	EA = 1;
+	ET0 = 1;
+	TR0 = 1;
+}
+	
+//função delay para o LCD
+void atraso() //50us
+{
+	TF0 = 0;
+	TMOD = 2;
+	TL0 = 210;  //Ttimer=1.085 us --> 46clocks --> 256-46=210.
+	TR0 = 1;
+	while (TF0==0);
+}
+
+//função delay para o lcd 
+void atraso_display() //2ms = 40*atraso
+{
+	int disp_delay=0;
+	do{
+	atraso();
+	disp_delay ++;
+	}while (disp_delay==40);
+}
+	
+void atraso_debounce() //10ms = 200*atraso
+{
+	int debounce_delay=0;
+	do{
+	atraso();
+	debounce_delay ++;
+	}while (debounce_delay==200);
+}	
+	
 char debouncing(char PINO)
 {
     char key_valid;
@@ -111,7 +151,7 @@ char debouncing(char PINO)
 
     for(count=0; count<3; count++)
     {
-        delay();   !!!!!!!!!!!!!!!!!!!!!!
+        atraso_debounce();
 
         key_now = PINO;
 
@@ -187,22 +227,43 @@ void escreve_lcd(char TECLA)
 	atraso(); 
 }
 
-//função delay para o LCD
-void atraso() //50us
+	
+	
+//setup timer 2 - 10ms
+void setup_timer2()
 {
-	TF0 = 0;
-	TMOD = 2;
-	TL0 = 210;  //Ttimer=1.085 us --> 46clocks --> 256-46=210.
-	TR0 = 1;
-	while (TF0==0);
+	T2CON = 4;  //recarga automática - nao sei se tá certo, MESMO TR2
+	RCAP2H =0xB8;
+	RCAP2L = 0x00;
+	TH1= RCAP2H;
+	TL1= RCAP2L;
+	EA = 1; //enable global interrupt.
+	//TR2=1; // run control flag set by software
+	ET2=1; // enable interrupt timer 2
 }
 
-//função delay para o lcd 
-void atraso_display() //2ms = 40*atraso
+//relógio usando o timer 2
+void relogio () interrupt 5
 {
-	int disp_delay=0;
-	do{
-	atraso();
-	disp_delay ++;
-	}while (disp_delay==40);
+	int dezmili, segundos, minutos, horas = 0; //iniciar no .c principal
+	milissegundos++;
+	if(dezmili == 100)//entra a cada segundo
+	{
+		dezmili = 0;
+		segundos ++;
+		if(segundos == 60)
+		{
+			segundos = 0;
+			minutos ++;
+			if(minutos == 60)
+			{
+				minutos = 0;
+				horas ++;
+				if(horas == 24)
+					horas=0;
+			}
+		}
+	}
+	TF2 = 0;
 }
+
