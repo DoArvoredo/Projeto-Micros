581162484823ec6a75fd158e7c73dc77cb0adb3c
DoArvoredo <39414989+DoArvoredo@users.noreply.github.com>
2018-05-18 13:52:54 -0300

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS
+#define FUNCTIONS
+
+char varredura_teclado(void);
+char debouncing(char PINO);
+char teclado();
+void LCD_init (void);
+void escreve_lcd(char TECLA);
+
+
+#endif // FUNCTIONS
